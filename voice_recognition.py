b54dd6c00450780d6c4b539cb045f0d2928805ff
Sterling <sterling.nesbitt@gmail.com>
2022-05-15 13:09:24 -0400

    speech_recognition. needs pyaudio as a dependency.

---
+++ voice_recognition.py
@@ -0,0 +1,14 @@
+# pip install pipwin
+# pipwin install pyaudio
+import speech_recognition as sr
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Talk")
+    audio_text = r.listen(source)
+    print("Time over, thanks")
+
+try:
+    print("Text: " + r.recognize_google(audio_text))
+except:
+    print("Sorry, I did not get that")
