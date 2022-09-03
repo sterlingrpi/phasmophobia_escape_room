4419989144cfa91d6da6ba4ed77490a902692755
Sterling <sterling.nesbitt@gmail.com>
2022-09-03 09:17:33 -0400

    added all the other outlet switches

--- rf_transmitter/rf_transmitter.ino
+++ rf_transmitter/rf_transmitter.ino
@@ -1,34 +1,54 @@
 //https://github.com/sui77/rc-switch
-//    on      off
-//ch1 1208780 1208772
-//ch2 1208778 1208770
-//ch3 1208777 1208769
-//ch4 1208781 1208773
-//ch5 1208779 1208771
+//    off     on
+//ch1 1208772 1208780 
+//ch2 1208770 1208778 
+//ch3 1208769 1208777 
+//ch4 1208773 1208781
+//ch5 1208771 1208779 
 
 #include <RCSwitch.h>
 
 RCSwitch mySwitch = RCSwitch();
-char incoming_char = 0; 
+char incoming_char; 
 
 void setup() {
   mySwitch.enableTransmit(10);  // Using Pin #10
   mySwitch.setPulseLength(197); // measured using receive
-  mySwitch.send(1208770, 24); //off
-//  mySwitch.send(1208778, 24); //on
   Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     incoming_char = Serial.read();
-    
-    if (incoming_char == 48) {
-      mySwitch.send(1208770, 24); //off
+    if (incoming_char == '0') {
+      mySwitch.send(1208772, 24); //ch1 off
     }
-
-    if (incoming_char == 49) {
-      mySwitch.send(1208778, 24); //on
+    if (incoming_char == '1') {
+      mySwitch.send(1208780, 24); //ch1 on
+    }
+    if (incoming_char == '2') {
+      mySwitch.send(1208770, 24); //ch2 off
+    }
+    if (incoming_char == '3') {
+      mySwitch.send(1208778, 24); //ch2 on
+    }
+    if (incoming_char == '4') {
+      mySwitch.send(1208769, 24); //ch3 off
+    }
+    if (incoming_char == '5') {
+      mySwitch.send(1208777, 24); //ch3 on
+    }
+    if (incoming_char == '6') {
+      mySwitch.send(1208773, 24); //ch4 off
+    }
+    if (incoming_char == '7') {
+      mySwitch.send(1208781, 24); //ch4 on
+    }
+    if (incoming_char == '6') {
+      mySwitch.send(1208771, 24); //ch5 off
+    }
+    if (incoming_char == '7') {
+      mySwitch.send(1208779, 24); //ch5 on
     }
   }
 }
