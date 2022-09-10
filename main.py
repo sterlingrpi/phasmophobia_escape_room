3da4454a809568889a2b20b9910eaa4dde76bc8a
Sterling <sterling.nesbitt@gmail.com>
2022-09-10 11:00:57 -0400

    updating image/object detection and voice recognition in a thread and putting in a queue. Main thread displays updated image when there is an image in the image queue

--- main.py
+++ main.py
@@ -1,81 +1,87 @@
-# importing tkinter module
-from tkinter import Tk, mainloop, HORIZONTAL, Text, Canvas
-from tkinter.ttk import Progressbar, Button
+import threading
+import queue
 import time
+import random
+from object_detection import Camera
+from tkinter import Tk, mainloop, HORIZONTAL, Text, Canvas
+from PIL import Image, ImageTk
+from tkinter.ttk import Progressbar, Button
 
-# creating tkinter window
+from cv2 import VideoCapture, cvtColor, COLOR_BGR2RGB
+
+#initialize parameters
+hunting = True
+
+#initialize queues
+image_queue = queue.Queue()
+text_queue = queue.Queue()
+
+#initialize camera
+camera = Camera()
+
+#define threads
+def video_thread():
+    while True:
+        if image_queue.empty():
+            camera.capture_image()
+            if hunting:
+                camera.run_detector()
+            image = camera.get_image()
+            image_queue.put(image)
+        else:
+            time.sleep(1)
+
+def voice_thread():
+    while True:
+        if text_queue.empty():
+            text = 'text ' + str(random.randint(0, 10))
+            text_queue.put(text)
+        else:
+            time.sleep(10)
+
+#start threads
+threading.Thread(target=video_thread, daemon=True).start()
+threading.Thread(target=voice_thread, daemon=True).start()
+time.sleep(1)
+
+
+# creating GUI
 root = Tk()
 root.geometry('800x600')
+ghost_canvas = Canvas(root, width=800, height=800)  # Create 200x200 Canvas widget
 
-# Progress bar widget
-progress = Progressbar(root, orient=HORIZONTAL,
-                       length=100, mode='determinate')
-progress['value'] = 100
-
-ghost_canvas = Canvas(root, width=200, height=200)  # Create 200x200 Canvas widget
+# initializing image
+image = ImageTk.PhotoImage(Image.open("cam.jpg"))
+image_canvas = ghost_canvas.create_image(20,
+                                        20,
+                                        anchor='nw',
+                                        image=image,
+                                        )
 ghost_canvas.pack()
 
-#lamp = ghost_canvas.create_oval(50, 50, 100, 100, fill='yellow')  # Create a circle on the Canvas
-import serial
-serialPort = serial.Serial(port="COM3",
-                           baudrate=9600,
-                           bytesize=8,
-                           timeout=2,
-                           stopbits=serial.STOPBITS_ONE
-                           )
-print('initializing...')
-time.sleep(3)
-serialPort.write(b'3')
-
-def on():
-    serialPort.write(b'3')
-
-def off():
-    serialPort.write(b'2')
-
-def blink():
-    print('do the blink')
-    for _ in range(2):
-        #ghost_canvas.itemconfig(lamp, fill='gray')
-        #root.update_idletasks()
-        serialPort.write(b'2')
+def update_image(event):
+    if image_queue.empty() and text_queue.empty():
+        print('all queues empty zzz...')
         time.sleep(0.5)
-        #ghost_canvas.itemconfig(lamp, fill='yellow')
-        #root.update_idletasks()
-        serialPort.write(b'3')
-        time.sleep(0.5)
+    else:
+        if not image_queue.empty():
+            global ghost_canvas
+            global image_canvas
+            global image
 
-def spirit_box_loop():
-    import speech_recognition as sr
-    r = sr.Recognizer()
-    while True:
-        with sr.Microphone() as source:
-            print("Talk")
-            audio_text = r.listen(source)
-            print("Time over, thanks")
-        try:
-            text = r.recognize_google(audio_text)
+            image = image_queue.get()
+
+            image = cvtColor(image, COLOR_BGR2RGB)  # to RGB
+            image = Image.fromarray(image)  # to PIL format
+            image = ImageTk.PhotoImage(image)  # to ImageTk format
+
+            ghost_canvas.itemconfig(image_canvas, image=image)  # change the displayed picture
+            ghost_canvas.pack()
+
+            print('updating image')
+        if not text_queue.empty():
+            text = text_queue.get()
             print(text)
-            if 'sign' in text or 'show' in text:
-                blink()
-        except:
-            print("Sorry, I did not get that")
 
-def update_sanity():
-    sanity_values = [100, 80, 60,40, 20, 0]
-    for sanity_value in sanity_values:
-        progress['value'] = sanity_value
-        root.update_idletasks()
-        time.sleep(1)
-
-progress.pack(pady=10)
-
-# This button will initialize
-# the progress bar
-Button(root, text='Sanity drain', command=update_sanity).pack(pady=10)
-Button(root, text='Spirit box on', command=spirit_box_loop).pack(pady=10)
-Button(root, text='on', command=on).pack(pady=10)
-Button(root, text='off', command=off).pack(pady=10)
-
-# infinite loop
-mainloop()
+root.bind('<Right>', update_image) # on right arrow key display random note
+ghost_canvas.mainloop()
