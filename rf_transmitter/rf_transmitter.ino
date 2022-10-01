59aaf4610315f4a611b76572b9d60db00c2a7bb3
Sterling <sterling.nesbitt@gmail.com>
2022-10-01 12:57:08 -0400

    added support for up to 10 outlet switches

--- rf_transmitter/rf_transmitter.ino
+++ rf_transmitter/rf_transmitter.ino
@@ -1,13 +1,16 @@
 //https://github.com/sui77/rc-switch
-//    off     on
-//ch1 1208772 1208780 
-//ch2 1208770 1208778 
-//ch3 1208769 1208777 
-//ch4 1208773 1208781
-//ch5 1208771 1208779 
+//    on      off
+//ch1 1208780 1208772
+//ch2 1208778 1208770
+//ch3 1208777 1208769
+//ch4 1208781 1208773
+//ch5 1208779 1208771
 
-//ch6 1208774 1208782
-//ch7 1208768 1208776
+//ch6 774524 774516
+//ch7 774522 774514
+//ch8 774521 774513
+//ch9 774525 774517
+//ch10 774523 774515
 
 #include <RCSwitch.h>
 
@@ -24,46 +27,64 @@ void loop() {
   if (Serial.available() > 0) {
     incoming_char = Serial.read();
     if (incoming_char == '0') {
-      mySwitch.send(1208772, 24); //ch1 off
-    }
-    if (incoming_char == '1') {
       mySwitch.send(1208780, 24); //ch1 on
     }
-    if (incoming_char == '2') {
-      mySwitch.send(1208770, 24); //ch2 off
+    if (incoming_char == '1') {
+      mySwitch.send(1208772, 24); //ch1 off
     }
-    if (incoming_char == '3') {
+    if (incoming_char == '2') {
       mySwitch.send(1208778, 24); //ch2 on
     }
-    if (incoming_char == '4') {
-      mySwitch.send(1208769, 24); //ch3 off
+    if (incoming_char == '3') {
+      mySwitch.send(1208770, 24); //ch2 off
     }
-    if (incoming_char == '5') {
+    if (incoming_char == '4') {
       mySwitch.send(1208777, 24); //ch3 on
     }
-    if (incoming_char == '6') {
-      mySwitch.send(1208773, 24); //ch4 off
+    if (incoming_char == '5') {
+      mySwitch.send(1208769, 24); //ch3 off
     }
-    if (incoming_char == '7') {
+    if (incoming_char == '6') {
       mySwitch.send(1208781, 24); //ch4 on
     }
-    if (incoming_char == '8') {
-      mySwitch.send(1208771, 24); //ch5 off
+    if (incoming_char == '7') {
+      mySwitch.send(1208773, 24); //ch4 off
     }
-    if (incoming_char == '9') {
+    if (incoming_char == '8') {
       mySwitch.send(1208779, 24); //ch5 on
     }
+    if (incoming_char == '9') {
+      mySwitch.send(1208771, 24); //ch5 off
+    }
     if (incoming_char == 'a') {
-      mySwitch.send(1208774, 24); //ch6 off
+      mySwitch.send(774524, 24); //ch6 on
     }
     if (incoming_char == 'b') {
-      mySwitch.send(1208782, 24); //ch6 on
+      mySwitch.send(774516, 24); //ch6 off
     }
     if (incoming_char == 'c') {
-      mySwitch.send(1208768, 24); //ch7 off
+      mySwitch.send(774522, 24); //ch7 on
     }
     if (incoming_char == 'd') {
-      mySwitch.send(1208776, 24); //ch7 on
+      mySwitch.send(774514, 24); //ch7 off
+    }
+    if (incoming_char == 'e') {
+      mySwitch.send(774521, 24); //ch8 on
+    }
+    if (incoming_char == 'f') {
+      mySwitch.send(774513, 24); //ch8 off
+    }
+    if (incoming_char == 'g') {
+      mySwitch.send(774525, 24); //ch9 on
+    }
+    if (incoming_char == 'h') {
+      mySwitch.send(774517, 24); //ch9 off
+    }
+    if (incoming_char == 'i') {
+      mySwitch.send(774523, 24); //ch10 on
+    }
+    if (incoming_char == 'j') {
+      mySwitch.send(774515, 24); //ch10 off
     }
   }
 }
--- serial_control.py
+++ serial_control.py
@@ -12,64 +12,90 @@ serialPort = serial.Serial(port="COM3",
 print('initializing...')
 time.sleep(2)
 
-def ch1_off():
-    print('ch1 off')
-    serialPort.write(b'0')
 def ch1_on():
     print('ch1 on')
+    serialPort.write(b'0')
+def ch1_off():
+    print('ch1 off')
     serialPort.write(b'1')
-def ch2_off():
-    print('ch2 off')
-    serialPort.write(b'2')
 def ch2_on():
     print('ch2 on')
+    serialPort.write(b'2')
+def ch2_off():
+    print('ch2 off')
     serialPort.write(b'3')
-def ch3_off():
-    print('ch3 off')
-    serialPort.write(b'4')
 def ch3_on():
     print('ch3 on')
+    serialPort.write(b'4')
+def ch3_off():
+    print('ch3 off')
     serialPort.write(b'5')
-def ch4_off():
-    print('ch4 off')
-    serialPort.write(b'6')
 def ch4_on():
     print('ch4 on')
+    serialPort.write(b'6')
+def ch4_off():
+    print('ch4 off')
     serialPort.write(b'7')
-def ch5_off():
-    print('ch5 off')
-    serialPort.write(b'8')
 def ch5_on():
     print('ch5 on')
+    serialPort.write(b'8')
+def ch5_off():
+    print('ch5 off')
     serialPort.write(b'9')
-def ch6_off():
-    print('ch6 off')
-    serialPort.write(b'a')
 def ch6_on():
     print('ch6 on')
+    serialPort.write(b'a')
+def ch6_off():
+    print('ch6 off')
     serialPort.write(b'b')
-def ch7_off():
-    print('ch7 off')
-    serialPort.write(b'c')
 def ch7_on():
     print('ch7 on')
+    serialPort.write(b'c')
+def ch7_off():
+    print('ch7 off')
     serialPort.write(b'd')
+def ch8_on():
+    print('ch8 on')
+    serialPort.write(b'e')
+def ch8_off():
+    print('ch8 off')
+    serialPort.write(b'f')
+def ch9_on():
+    print('ch9 on')
+    serialPort.write(b'g')
+def ch9_off():
+    print('ch9 off')
+    serialPort.write(b'h')
+def ch10_on():
+    print('ch10 on')
+    serialPort.write(b'i')
+def ch10_off():
+    print('ch10 off')
+    serialPort.write(b'j')
 
 root = Tk()
-Button(root, text='ch1 off', command=ch1_off).pack(pady=10)
-Button(root, text='ch1 on', command=ch1_on).pack(pady=10)
-Button(root, text='ch2 off', command=ch2_off).pack(pady=10)
-Button(root, text='ch2 on', command=ch2_on).pack(pady=10)
-Button(root, text='ch3 off', command=ch3_off).pack(pady=10)
-Button(root, text='ch3 on', command=ch3_on).pack(pady=10)
-Button(root, text='ch4 off', command=ch4_off).pack(pady=10)
-Button(root, text='ch4 on', command=ch4_on).pack(pady=10)
-Button(root, text='ch5 off', command=ch5_off).pack(pady=10)
-Button(root, text='ch5 on', command=ch5_on).pack(pady=10)
-Button(root, text='ch6 off', command=ch6_off).pack(pady=10)
-Button(root, text='ch6 on', command=ch6_on).pack(pady=10)
-Button(root, text='ch7 off', command=ch7_off).pack(pady=10)
-Button(root, text='ch7 on', command=ch7_on).pack(pady=10)
+padx = 10
+pady =10
+Button(root, text='ch1 on', command=ch1_on).grid(row=1, column=1, padx=padx, pady=pady)
+Button(root, text='ch1 off', command=ch1_off).grid(row=1, column=2, padx=padx, pady=pady)
+Button(root, text='ch2 on', command=ch2_on).grid(row=2, column=1, padx=padx, pady=pady)
+Button(root, text='ch2 off', command=ch2_off).grid(row=2, column=2, padx=padx, pady=pady)
+Button(root, text='ch3 on', command=ch3_on).grid(row=3, column=1, padx=padx, pady=pady)
+Button(root, text='ch3 off', command=ch3_off).grid(row=3, column=2, padx=padx, pady=pady)
+Button(root, text='ch4 on', command=ch4_on).grid(row=4, column=1, padx=padx, pady=pady)
+Button(root, text='ch4 off', command=ch4_off).grid(row=4, column=2, padx=padx, pady=pady)
+Button(root, text='ch5 on', command=ch5_on).grid(row=5, column=1, padx=padx, pady=pady)
+Button(root, text='ch5 off', command=ch5_off).grid(row=5, column=2, padx=padx, pady=pady)
+Button(root, text='ch6 on', command=ch6_on).grid(row=6, column=1, padx=padx, pady=pady)
+Button(root, text='ch6 off', command=ch6_off).grid(row=6, column=2, padx=padx, pady=pady)
+Button(root, text='ch7 on', command=ch7_on).grid(row=7, column=1, padx=padx, pady=pady)
+Button(root, text='ch7 off', command=ch7_off).grid(row=7, column=2, padx=padx, pady=pady)
+Button(root, text='ch8 on', command=ch8_on).grid(row=8, column=1, padx=padx, pady=pady)
+Button(root, text='ch8 off', command=ch8_off).grid(row=8, column=2, padx=padx, pady=pady)
+Button(root, text='ch9 on', command=ch9_on).grid(row=9, column=1, padx=padx, pady=pady)
+Button(root, text='ch9 off', command=ch9_off).grid(row=9, column=2, padx=padx, pady=pady)
+Button(root, text='ch10 on', command=ch10_on).grid(row=10, column=1, padx=padx, pady=pady)
+Button(root, text='ch10 off', command=ch10_off).grid(row=10, column=2, padx=padx, pady=pady)
 mainloop()
 
 serialPort.close()
