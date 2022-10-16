6dd132b6f3974c63549d2a94a3ee6f7cdd163f97
Sterling <sterling.nesbitt@gmail.com>
2022-10-16 10:41:10 -0400

    hunt sequence, control animated ghost, temperature meter, throwing object, emf, stop and start threads, multiple sound devices

--- house.py
+++ house.py
@@ -6,6 +6,11 @@ import threading
 
 class House:
     def __init__(self):
+        self.mixer = mixer
+        # 'Headphones (Realtek(R) Audio)', 'Speakers (Realtek(R) Audio)', 'ASUS VA24E (NVIDIA High Definition Audio)', 'Speakers (Jabra EVOLVE LINK)'
+        self.mixer_speaker = 'Headphones (Realtek(R) Audio)'
+        self.mixer_sb7 = 'Headphones (Realtek(R) Audio)'
+
         self.serialPort = serial.Serial(port="COM3",
                                         baudrate=9600,
                                         bytesize=8,
@@ -16,7 +21,6 @@ class House:
         time.sleep(2)
         self.serialPort.write(b'3')
 
-        self.mixer = mixer.init()
         self.outlet_1 = [b'0', b'1']
         self.outlet_2 = [b'2', b'3']
         self.outlet_3 = [b'4', b'5']
@@ -29,22 +33,30 @@ class House:
         self.outlet_9 = [b'g', b'h']
         self.outlet_10 = [b'i', b'j']
 
-        self.lamps = [self.outlet_1, self.outlet_2]
+        self.lamps = [self.outlet_1, self.outlet_2, self.outlet_9]
         self.book_outlet = self.outlet_4
-        self.fan_outlet = self.outlet_5
-        self.emf_outlet = self.outlet_6
-        self.fingies_outout = self.outlet_7
-        self.solenoid_outlet = self.outlet_8
+        self.emf_outlet = self.outlet_5
+        self.throw_outlet = self.outlet_6
+        self.fan_outlet = self.outlet_7
+        self.fingies_outout = self.outlet_8
         self.hunt_outlets = [self.outlet_1,
                              self.outlet_2,
                              self.outlet_3,
                              self.outlet_4,
-                             self.outlet_5]
-    def on(self, outlet):
-        self.serialPort.write(outlet[0])
+                             self.outlet_5,
+                             self.outlet_7,
+                             self.outlet_8]
 
-    def off(self, outlet):
-        self.serialPort.write(outlet[1])
+        self.ghost_chars = [b'k', b'l']
+
+        for outlet in self.lamps:
+            self.on(outlet)
+
+    def on(self, char):
+        self.serialPort.write(char[0])
+
+    def off(self, char):
+        self.serialPort.write(char[1])
 
     def blink(self):
         print('do the blink')
@@ -66,26 +78,74 @@ class House:
         threading.Thread(target=self.interaction_thread,
                          args=(self.book_outlet, 20),
                          daemon=True).start()
+
+    def emf(self):
+        threading.Thread(target=self.interaction_thread,
+                         args=(self.emf_outlet, 10),
+                         daemon=True).start()
+
+    def fingies(self):
+        threading.Thread(target=self.interaction_thread,
+                         args=(self.fingies_outout, 20),
+                         daemon=True).start()
+
     def fan(self):
         threading.Thread(target=self.interaction_thread,
                          args=(self.fan_outlet, 10),
                          daemon=True).start()
 
-    def old(self):
-        self.mixer.music.load('sounds/adult.mp3')
-        self.mixer.music.play()
+    def throw(self):
+        threading.Thread(target=self.interaction_thread,
+                         args=(self.throw_outlet, 2),
+                         daemon=True).start()
 
-    def hunt_sequence(self, seconds):
-        time_end = time.time() + seconds
-        while time_end > time.time():
+    def play_sound(self, file):
+        mixer.init(devicename=self.mixer_speaker)
+        mixer.music.load(file)
+        mixer.music.play()
+        while mixer.music.get_busy():
+            time.sleep(0.1)
+        mixer.quit()
+
+    def sb7_sound(self, file):
+        mixer.init(devicename=self.mixer_sb7)
+        mixer.music.load(file)
+        mixer.music.play()
+        while mixer.music.get_busy():
+            time.sleep(0.1)
+        mixer.quit()
+
+    def sound_thread(self, file, stop):
+        mixer.init(devicename=self.mixer_speaker)
+        mixer.music.load(file)
+        while True:
+            if not self.mixer.music.get_busy():
+                mixer.music.play()
+            else:
+                time.sleep(0.1)
+            if stop():
+                break
+
+    def hunt_sequence(self, duration):
+        hunt_end = time.time() + duration
+        stop = False
+        gurgle_tread = threading.Thread(target=self.sound_thread,
+                                        daemon=True,
+                                        args=('sounds/gurgle.mp3', lambda: stop,))
+        gurgle_tread.start()
+        self.on(self.ghost_chars)
+        while hunt_end > time.time():
             for outlet in self.hunt_outlets:
                 if random.choice(['on', 'off']) == 'on':
                     self.on(outlet)
                 else:
                     self.off(outlet)
             time.sleep(1)
+        stop = True
+        gurgle_tread.join()
         for outlet in self.hunt_outlets:
             self.off(outlet)
+        self.off(self.ghost_chars)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import threading
 import time
-from tkinter import Tk, mainloop, HORIZONTAL, Text, Canvas, Label, Frame
-from tkinter.ttk import Progressbar, Button
+from tkinter import Tk, mainloop, HORIZONTAL, VERTICAL, Text, Canvas, Label, Frame, Button
+from tkinter.ttk import Progressbar
 from PIL import Image, ImageTk
 from pygame import mixer
 from cv2 import VideoCapture, cvtColor, COLOR_BGR2RGB
+import random
 from object_detection import Camera
 from voice_recognition import Voice
 from house import House
 
 #initialize parameters
-hunting = True
+time_to_hunt = 60*5
+hunt_duration = 30
+grace_period = 15
+run_threads = True
 text = ''
+temperature = 68
 
 #initialize objects
 camera = Camera()
@@ -21,7 +26,7 @@ mixer.init()
 
 # creating GUI
 ghost_window = Tk()
-ghost_window.geometry('800x600')
+ghost_window.geometry('900x600')
 
 # initializing image
 ghost_frame = Frame(ghost_window, width=1600, height=900)
@@ -29,93 +34,151 @@ ghost_frame.grid(row=0, column=0, padx=10, pady=2)
 image_label = Label(ghost_frame)
 image_label.grid(row=0, column=0)
 
+# define hunt function
+def hunt(duration, grace):
+    house.hunt_sequence(duration=duration)
+    hunt_end = time.time() + duration
+    grace_end = time.time() + grace
+    while hunt_end > time.time():
+        if time.time() > grace_end:
+            if camera.are_you_near():
+                print('you are dead')
+                house.play_sound('sounds/death.mp3')
+                camera.display_image()
+                break
+        time.sleep(0.1)
+
 # define threads
 def update_image():
     #https://stackoverflow.com/questions/32342935/using-opencv-with-tkinter
-    camera.capture_image()
-    camera.run_detector()
-    image = camera.get_image()
+    if run_threads:
+        camera.capture_image()
+        camera.run_detector()
+        image = camera.get_image()
 
-    imgtk = ImageTk.PhotoImage(Image.fromarray(cvtColor(image, COLOR_BGR2RGB)))
+        imgtk = ImageTk.PhotoImage(Image.fromarray(cvtColor(image, COLOR_BGR2RGB)))
 
-    image_label.imgtk = imgtk
-    image_label.configure(image=imgtk)
+        image_label.imgtk = imgtk
+        image_label.configure(image=imgtk)
     image_label.after(10, update_image)
 
 def voice_thread():
     global text
     # initializing text label
     text_label = Label(ghost_window, text="")
-    text_label.place(x=70, y=90)
+    text_label.place(x=20, y=20)
     while True:
-        text = voice.get_text()
-        if text:
-            text_label.configure(text=text)
-
-def sound_thread(file, seconds):
-    time_end = time.time() + seconds
-    while time_end > time.time():
-        print('playing sound')
-        mixer.music.load(file)
-        mixer.music.play()
+        if run_threads:
+            text = voice.get_text()
+            if text:
+                text_label.configure(text=text)
+        else:
+            time.sleep(1)
 
 def interatction_thread():
     global text
+    global temperature
     while True:
-        try:
-            if 'show' in text or 'sign' in text:
-                house.blink()
-                text = ''
-            if 'fan' in text:
-                house.fan()
-                text = ''
-            if 'book' in text:
-                house.book()
-                text = ''
-            if 'where' in text:
-                text = ''
-                if camera.are_you_near():
-                    mixer.music.load('sounds/behind.mp3')
-                    mixer.music.play()
-                else:
-                    mixer.music.load('sounds/away.mp3')
-                    mixer.music.play()
-            if 'old' in text:
-                mixer.music.load('sounds/adult.mp3')
-                mixer.music.play()
-                text = ''
-            if 'hide and seek' in text:
-                hunt_seconds = 15
-                # threading.Thread(target=sound_thread,
-                #                  args=('sounds/gurgle.mp3', hunt_seconds),
-                #                  daemon=True).start()
-                mixer.music.load('sounds/gurgle.mp3')
-                mixer.music.play(-1)
-                house.hunt_sequence(hunt_seconds)
-        except:
-            pass
-        time.sleep(0.1)
+        if run_threads:
+            try:
+                if 'show' in text or 'sign' in text:
+                    text = ''
+                    house.blink()
+                if 'fan' in text:
+                    text = ''
+                    house.fan()
+                if 'book' in text:
+                    text = ''
+                    house.book()
+                if 'EMF' in text:
+                    text = ''
+                    house.emf()
+                if 'finger' in text:
+                    house.fingies()
+                    text = ''
+                if 'throw' in text:
+                    house.throw()
+                    text = ''
+                if 'where' in text:
+                    text = ''
+                    if camera.are_you_near():
+                        house.sb7_sound('sounds/behind.mp3')
+                    else:
+                        house.sb7_sound('sounds/away.mp3')
+                if ' old ' in text:
+                    text = ''
+                    house.sb7_sound('sounds/adult.mp3')
+                if 'cold' in text:
+                    text = ''
+                    temperature = 32
+                if 'heat' in text:
+                    text = ''
+                    temperature = 68
+                if 'hide and seek' in text:
+                    text = ''
+                    hunt(duration=hunt_duration, grace=grace_period)
+            except:
+                pass
+            time.sleep(0.1)
+        else:
+            time.sleep(1)
 
 def sanity_thread():
     # initializing sanity meter
-    sanity_label = Label(ghost_window, text="sanity")
+    sanity_label = Label(ghost_window, text="sanity (%)")
     sanity_label.grid(row=1, column=0)
     sanity_bar = Progressbar(ghost_window,
                              orient=HORIZONTAL,
                              length=500,
                              mode='determinate', )
-    sanity_bar['value'] = 100
     sanity_bar.grid(row=2, column=0)
+    starting_sanity = 100
+    sanity_bar['value'] = starting_sanity
+
     while True:
-        time.sleep(3)
-        sanity_bar['value'] = sanity_bar['value'] -1
-        if sanity_bar['value'] <= 0:
-            print('start hunt sequence')
-            sanity_bar['value'] = 100
+        if run_threads:
+            time.sleep(time_to_hunt/starting_sanity)
+            sanity_bar['value'] = sanity_bar['value'] -1
+            if sanity_bar['value'] <= 0:
+                hunt(duration=hunt_duration, grace=grace_period)
+                sanity_bar['value'] = starting_sanity
+        else:
+            time.sleep(1)
+
+def thermo_thread():
+    # initialize thermometer
+    global temperature
+    thermo_label = Label(ghost_window, text="temperature (F)")
+    thermo_label.grid(row=0, column=2)
+    thermo_bar = Progressbar(ghost_window,
+                             orient=VERTICAL,
+                             length=500,
+                             mode='determinate', )
+    thermo_bar.grid(row=0, column=1)
+    thermo_bar['value'] = 68
+    while True:
+        if run_threads:
+            thermo_bar['value'] = temperature + random.random()*2 -1
+            time.sleep(0.5)
+        else:
+            time.sleep(1)
 
 # start threads
 update_image()
 threading.Thread(target=voice_thread, daemon=True).start()
 threading.Thread(target=interatction_thread, daemon=True).start()
 threading.Thread(target=sanity_thread, daemon=True).start()
+threading.Thread(target=thermo_thread, daemon=True).start()
+
+def start_game():
+    global run_threads
+    run_threads = True
+
+def stop_game():
+    global run_threads
+    run_threads = False
+
+Button(ghost_window, text='start game', command=start_game).grid(row=3, column=0)
+Button(ghost_window, text='stop game', command=stop_game).grid(row=3, column=1)
+
 ghost_window.mainloop()
